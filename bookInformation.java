a4551ec80680cb8fe03ad423ff20b4d25d9ee1db
Jonathan <90427766+JonathanAsf@users.noreply.github.com>
2025-03-28 21:53:59 -0300

    Create bookInformation.java

---
+++ bookInformation.java
@@ -0,0 +1,14 @@
+public class FavoriteBook {
+    public static void main(String args[]) {
+        String title = "I Robot";
+        String author = "Isaac Asimov";
+        int pages = 320;
+        double price = 20.50;
+
+        System.out.println("Title: " + title);
+        System.out.println("Author Name: " + author);
+        System.out.println("Quantity of pages :" + pages);
+        System.out.println("Price: U$" + price);
+
+    }
+}
